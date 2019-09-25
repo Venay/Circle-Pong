0528cd593d1ffcff2b2d668ab766b52f5226fd17
Venay <silver0snake@gmail.com>
2019-09-25 08:15:58 +0100

    t

---
+++ Assets/testFolder/testScript.cs
@@ -0,0 +1,36 @@
+<<<<<<< HEAD
+=======
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testScript : MonoBehaviour
+{
+	public Transform square, bin;
+	bool inBin = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+		if (Input.GetMouseButtonDown(0))
+		{
+			if (inBin)
+			{
+				square.SetParent(transform);
+				inBin = !inBin;
+			}
+			else
+			{
+				square.SetParent(bin);
+				inBin = !inBin;
+			}
+		}
+
+
+
+
+
+    }
+}
+>>>>>>> parent of 07d542e... Correct Controls
